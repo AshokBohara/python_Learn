78896530ad3cda199b44f7d420731324d215845b
Ashok Bohara <boharaashok32@gmail.com>
2025-01-08 20:09:05 +0545

    3rd code of basic  python

---
+++ Basic/third.py
@@ -0,0 +1,4 @@
+course='engineering course' #here course is variable.
+#print (course.upper()) #print engineering in upper case =ENGINEERING.
+print(course.find('c')) #print the position of c in string engineering.
+print('course' in course) #checking the string course is there in string of first line if it is there it's print true else false
