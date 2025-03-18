89eb7adba3ea0b39d616181b01927ad8556f3843
Ashok Bohara <boharaashok32@gmail.com>
2025-03-18 21:35:18 +0545

    modified

--- Basic/4th.py
+++ Basic/4th.py
@@ -2,3 +2,8 @@
 birth_year= input("Enter your Birth year: ")
 age=2025-int(birth_year)
 print(age)
+
+#both code are correct ..
+#birth_year = int(input("Enter your Birth year: "))  # Convert the input to an integer
+#age = 2025 - birth_year  # Now the subtraction will work correctly
+#print(age)
