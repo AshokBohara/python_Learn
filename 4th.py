7f0aa77b7ca57b8e5ee171f2a69533ac010df298
Ashok Bohara <boharaashok32@gmail.com>
2026-04-28 20:47:26 +0545

    add python files

---
+++ 4th.py
@@ -0,0 +1,9 @@
+#Variables in python..
+birth_year= input("Enter your Birth year: ")
+age=2025-int(birth_year)
+print(age)
+
+#both code are correct ..
+#birth_year = int(input("Enter your Birth year: "))  # Convert the input to an integer
+#age = 2025 - birth_year  # Now the subtraction will work correctly
+#print(age)
---
+++ first.py
@@ -0,0 +1,12 @@
+#python learn...
+print("hello world!")
+name=input("what is your name? ")
+print("hello "+name)
+
+#Variables in python..
+birth_year = "2000"  # simulate input
+age = 2025 - int(birth_year)
+print(age)
+
+classA = 2
+print(classA)
